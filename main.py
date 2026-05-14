5e233385607ce28c93563f1958eddbfa5c29b08a
limin05030 <hanren2012@gmail.com>
2026-05-14 17:09:51 +0800

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 import wx
 from ui.app import DeveloperToolsApp
 
-# pip freeze > requirements.txt
+# pip freeze > requirements.txt （不要使用这个命令更新requirements.txt，有些库是 macOS 平台专有的）
 # pip install -r requirements.txt
 # pip install pyinstaller
 # git tag v1.0.0
